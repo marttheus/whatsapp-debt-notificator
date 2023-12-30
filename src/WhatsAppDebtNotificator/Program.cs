e95c181773c74d8101838efb3a0e15c6596710a2
Matheus Felipe <marttheusf@gmail.com>
2023-12-30 00:52:15 -0300

    feat: create initial worker project

---
+++ src/WhatsAppDebtNotificator/Program.cs
@@ -0,0 +1,14 @@
+namespace WhatsAppDebtNotificator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
