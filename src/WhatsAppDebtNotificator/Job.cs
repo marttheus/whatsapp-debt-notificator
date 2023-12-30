75063909be901a9bd48fcc5d52497246babaae9c
Matheus Felipe <marttheusf@gmail.com>
2023-12-30 01:53:01 -0300

    feat[wip]: create job

---
+++ src/WhatsAppDebtNotificator/Job.cs
@@ -0,0 +1,13 @@
+﻿namespace WhatsAppDebtNotificator;
+
+public class Job
+{
+    private readonly ILogger<Job> _logger;
+
+    public Job(ILogger<Job> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Execute(object state) => _logger.LogInformation("Job started at: {time}", DateTimeOffset.Now);
+}
